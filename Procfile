2e95786dfea53edfc6c69cdb9647d85b894f7566
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2019-10-24 19:29:51 -0300

    Last details

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: python3 server.py
-worker: python3 bot.py
+worker: python3 main.py
--- main.py
+++ main.py
@@ -13,16 +13,20 @@ while times.until(now_time):
     dispatch_time = times.time_to_send()
     while True:
         now_time = datetime.datetime.now().astimezone(times.local_timezone)
+        j = 0
         if now_time >= dispatch_time and times.pause(now_time):
-        # if now_time >= dispatch_time: # and times.pause(now_time):
             for i in range(len(followers_list)):
                 try:
-                    # auth.API.send_direct_message(followers_list[i], message.message())
-                    print("Sent")
+                    auth.API.send_direct_message(followers_list[i], message.message())
                 except tweepy.TweepError as TwitterError:
                     error = json.loads(TwitterError.response.text)
                     error_msg = 'Twitter error: ' + error['errors'][0]['message']
-                    # auth.API.send_direct_message(auth.user_ids['owner_twitter'], error_msg)
+                    auth.API.send_direct_message(auth.user_ids['owner_twitter'], error_msg)
+                j+=1
+
+            print("Sent for: ", j)
+            if j < len(followers_list):
+                auth.API.send_direct_message(auth.user_ids['owner_twitter'], "Can't sent for all followers, pls check")
             break
         else:
             # print(dispatch_time-now_time) # For testing purpose
