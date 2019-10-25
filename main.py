3fea5b66b55b87242d754a7816bfaef1d2e0fc8a
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2019-10-25 16:21:13 -0300

    Last bug, final test -> working vesion

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import times
 import tweepy
 import message
 import datetime
-# import authentication_h as auth
-import authentication as auth # For testing purpose
+import authentication_h as auth
+# import authentication as auth # For testing purpose
 
 followers_list = auth.API.followers_ids("@BotDaAgua")
 
 now_time = datetime.datetime.now().astimezone(times.local_timezone)
 
 first_time_flag = True
-
 while times.until(now_time):
     if first_time_flag == True:
-        auth.API.send_direct_message(auth.API.get_user(auth.user_ids['owner_twitter']).id, "It's running on heroku")
+        user = auth.API.get_user(auth.user_ids['owner_twitter']).id
+        auth.API.send_direct_message(user, "It's running on heroku")
         print("It's running on heroku")
         first_time_flag = False
 
@@ -25,8 +25,8 @@ while times.until(now_time):
         if now_time >= dispatch_time and times.pause(now_time):
             for i in range(len(followers_list)):
                 try:
-                    auth.API.get_user(followers_list[i]).id
-                    auth.API.send_direct_message(auth.API.get_user(followers_list[i]).id, message.message())
+                    user = auth.API.get_user(followers_list[i]).id
+                    auth.API.send_direct_message(user, message.message())
                 except tweepy.TweepError as TwitterError:
                     error = json.loads(TwitterError.response.text)
                     error_msg = 'Twitter error: ' + error['errors'][0]['message']
@@ -38,5 +38,5 @@ while times.until(now_time):
                 auth.API.send_direct_message(auth.API.get_user(auth.user_ids['owner_twitter']).id, "Can't sent for all followers, pls check")
             break
         else:
-            # print(dispatch_time-now_time) # For testing purpose
-            times.sleep1(60)
+            print(dispatch_time-now_time) # For testing purpose
+            times.sleep1(2)
