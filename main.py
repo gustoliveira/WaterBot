3b7b219308f7d4dc5db003384499ab82ca7c03bd
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2019-10-28 00:10:40 -0300

    Wasn't send to every follower

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ while True:
             times.sleep1(60*30)
 
     dispatch_time = times.time_to_send()
+    k = 0
     while True:
         now_time = datetime.datetime.now().astimezone(times.local_timezone)
         if now_time >= dispatch_time and times.pause(now_time):
@@ -34,10 +35,17 @@ while True:
                     # print('Sent to: ', auth.API.get_user(followers_list[i]).screen_name, message.message())
                 except tweepy.TweepError as TwitterError:
                     error = json.loads(TwitterError.response.text)
-                    error_msg = 'Twitter error: ' + error['errors'][0]['message']
-                    auth.API.send_direct_message(auth.user_ids['owner_twitter'], error_msg)
+                    error_code = 'Twitter error: ' + error['errors'][0]['code']
+                    if int(error_code) == 226:
+                        print(error['errors'][0]['message'])
+                        times.sleep1(60*30)
+                k += 1
+                print(k)
+
+            user = auth.API.get_user(auth.user_ids['owner_twitter']).id
+            auth.API.send_direct_message(user, k)
             break
         else:
-            # print(dispatch_time-now_time) # For testing purpose
-            times.sleep1(60)
+            print(dispatch_time-now_time) # For testing purpose
+            times.sleep1(3)
 
