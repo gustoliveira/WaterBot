b3f16fb101358e640799b33a208904c977309457
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2020-02-24 04:32:27 -0300

    Refactoring

--- main.py
+++ main.py
@@ -2,56 +2,54 @@ import json
 import times
 import tweepy
 import message
-import datetime
-import authentication_h as auth
+import authentication as auth
+
+from datetime import datetime
+from time import sleep
 
 OwnerUser = auth.API.get_user(auth.user_ids['owner_twitter']).id
 auth.API.send_direct_message(OwnerUser, "It's running on heroku")
-# print("It's running on heroku")
 
 while True:
+    # Try get owner id, if occur 226 error, "pause" the script for 30 minutes
+    # 226 error means the API thinks that it's a automated request
     try:
         auth.API.get_user(auth.user_ids['owner_twitter']).id
     except tweepy.TweepError as TwitterError:
         error = json.loads(TwitterError.response.text)
         error_code = error['errors'][0]['code']
         if int(error_code) == 226:
-            times.sleep1(60*30)
+            sleep(60*30)
 
     dispatch_time = times.time_to_send()
-    k = 0
+    follower_index = 0
     while True:
         now_time = datetime.datetime.now().astimezone(times.local_timezone)
         try:
             followers_list = auth.API.followers_ids("@BotDaAgua")
         except tweepy.TweepError as TwitterError:
-            error = json.loads(TwitterError.response.text)
-            error_code = error['errors'][0]['code']
-            if int(error_code) == 226 or int(error_code) == 88:
-                print(error['errors'][0]['message'])
-                sleeptime = 60*15
-                times.sleep1(sleeptime)
-            msg = 'FollowersUser_Twitter_Error: ' + error['errors'][0]['message']
-            print(msg)
-        if now_time >= dispatch_time and times.pause(now_time):
+            error_message('Followers_User Twitter Error: ')
+
+        if now_time >= dispatch_time and (not times.is_paused(now_time)):
             for i in range(len(followers_list)):
                 try:
                     auth.API.send_direct_message(followers_list[i], message.message())
-                    times.sleep1(2)
-                    # print('Sent to: ', auth.API.get_user(followers_list[i]).screen_name, message.message())
+                    sleep(2)
                 except tweepy.TweepError as TwitterError:
-                    error = json.loads(TwitterError.response.text)
-                    error_code = error['errors'][0]['code']
-                    if int(error_code) == 226 or int(error_code) == 88:
-                        print(error['errors'][0]['message'])
-                        sleeptime = 60*15
-                        times.sleep1(sleeptime)
-                    msg = 'SendToUSer_Twitter_Error: ' + error['errors'][0]['message']
-                    print(msg)
-                k += 1
+                    error_message('SendToUSer_Twitter_Error: ')
 
-            print("Sent to ", k, "users")
+                follower_index += 1
             break
         else:
-            times.sleep1(60)
+            sleep(60)
 
+def error_message(message):
+    error = json.loads(TwitterError.response.text)
+    error_code = error['errors'][0]['code']
+    # If occour a 226 or 88 error, "pause" for 15 minutes
+    # 88 error: Rate limit exceeded
+    if int(error_code) == 226 or int(error_code) == 88:
+        print(error['errors'][0]['message'])
+        sleep(60*15)
+    error_msg = message + error['errors'][0]['message']
+    print(error_msg)
--- times.py
+++ times.py
@@ -12,7 +12,7 @@ def today():
     year = str(now_time.astimezone(local_timezone).year)
     return (year + '/' + month + '/' + day)
 
-def pause(now_time):
+def is_paused(now_time):
     aux_ini = str(today() + ' 02:00')
     aux_fim = str(today() + ' 06:00')
     start_pause = datetime.strptime(aux_ini, '%Y/%m/%d %H:%M').astimezone(local_timezone)
@@ -24,10 +24,6 @@ def pause(now_time):
         return True
 
 def time_to_send():
-    delivery_time_ahead = datetime.now().astimezone(local_timezone) + timedelta(minutes=random.randrange(50, 60))
+    delivery_time_ahead = datetime.now().astimezone(local_timezone) + timedelta(minutes=random.randrange(55, 60))
     delivery_str = datetime.strftime(delivery_time_ahead, '%Y/%m/%d %H:%M')
     return datetime.strptime(delivery_str, '%Y/%m/%d %H:%M').astimezone(local_timezone)
-    # return datetime.now().astimezone(local_timezone)+timedelta(seconds=10) # For testing purpose
-
-def sleep1(i):
-    time.sleep(i)
