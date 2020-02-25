7aad628f52fb10af6379f48e57e41091f69f026b
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2020-02-24 21:59:36 -0300

    Final

--- main.py
+++ main.py
@@ -2,58 +2,62 @@ import json
 import times
 import tweepy
 import message
+import datetime
 import authentication as auth
 
 from datetime import datetime
 from time import sleep
 
-def error_message(message, text):
-    error = json.loads(text)
+def error_message(error):
     error_code = error['errors'][0]['code']
     # If occour a 226 or 88 error, "pause" for 28 minutes
     # 88 error: Rate limit exceeded
     if int(error_code) == 226 or int(error_code) == 88:
-        sleep(60*28)
-    error_msg = message + error['errors'][0]['message']
-    print(error_msg)
+        print(error['errors'][0]['message'])
+        sleep(60*30)
+    msg = 'Twitter error: ' + error['errors'][0]['message']
+    auth.API.send_direct_message(OwnerUser, msg)
+    print(msg)
 
-OwnerUser = auth.API.get_user(auth.user_ids['owner_twitter']).id
-auth.API.send_direct_message(OwnerUser, "It's alive")
 
-while True:
-    # Try get owner id, if occur 226 error, "pause" the script for 28 minutes
-    # 226 error means the API thinks that it's a automated request
-    try:
-        auth.API.get_user(auth.user_ids['owner_twitter']).id
-    except tweepy.TweepError as TwitterError:
-        error = json.loads(TwitterError.response.text)
-        error_code = error['errors'][0]['code']
-        if int(error_code) == 226:
-            sleep(60*28)
+def main():
+    OwnerUser = auth.API.get_user(auth.user_ids['owner_twitter']).id
+    auth.API.send_direct_message(OwnerUser, "It's running on heroku")
+    print("It's running on heroku")
 
-    dispatch_time = times.time_to_send()
-    follower_index = 0
     while True:
+        # Try get owner id, if occur 226 error, "pause" the script for 28 minutes
+        # 226 error means the API thinks that it's a automated request
+        try:
+            auth.API.get_user(auth.user_ids['owner_twitter']).id
+        except tweepy.TweepError as TwitterError:
+            error = json.loads(TwitterError.response.text)
+            error_message(error)
 
-        now_time = datetime.now().astimezone(times.local_timezone)
-
-        if now_time >= dispatch_time and not times.is_paused(now_time):
+        dispatch_time = times.time_to_send()
+        follower_index = 0
+        while True:
+            now_time = datetime.now().astimezone(times.local_timezone)
             try:
                 followers_list = auth.API.followers_ids("@BotDaAgua")
             except tweepy.TweepError as TwitterError:
-                error_message('Não conseguiu lista de seguidores\nList_of_followers Twitter Error: ',
-                                TwitterError.response.text)
-                continue
-            for i in range(len(followers_list)):
-                try:
-                    auth.API.send_direct_message(followers_list[i], message.message())
-                    sleep(2)
-                except tweepy.TweepError as TwitterError:
-                    error_message('SendToUSer_Twitter_Error: ',
-                                    TwitterError.response.text)
+                error = json.loads(TwitterError.response.text)
+                error_message(error)
 
-                follower_index += 1
-            break
+            if now_time >= dispatch_time and times.pause(now_time):
+                for i in range(len(followers_list)):
+                    try:
+                        auth.API.send_direct_message(followers_list[i], message.message())
+                        sleep(2)
+                        # print('Sent to: ', auth.API.get_user(followers_list[i]).screen_name, message.message())
+                    except tweepy.TweepError as TwitterError:
+                        error = json.loads(TwitterError.response.text)
+                        error_message(error)
 
-    sleep(60*5)
-    OwnerUser = auth.API.get_user(auth.user_ids['owner_twitter']).id
+                    follower_index += 1
+                break
+            else:
+                sleep(60)
+
+if __name__ == '__main__':
+    main()
--- times.py
+++ times.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 
 local_timezone = timezone('America/Bahia')
 
+
 def today():
     now_time = datetime.now()
     day = str(now_time.astimezone(local_timezone).day)
@@ -12,18 +13,22 @@ def today():
     year = str(now_time.astimezone(local_timezone).year)
     return (year + '/' + month + '/' + day)
 
-def is_paused(now_time):
+
+def pause(now_time):
     aux_ini = str(today() + ' 02:00')
     aux_fim = str(today() + ' 06:00')
-    start_pause = datetime.strptime(aux_ini, '%Y/%m/%d %H:%M').astimezone(local_timezone)
-    end_pause = datetime.strptime(aux_fim, '%Y/%m/%d %H:%M').astimezone(local_timezone)
+    start_pause = datetime.strptime(
+        aux_ini, '%Y/%m/%d %H:%M').astimezone(local_timezone)
+    end_pause = datetime.strptime(
+        aux_fim, '%Y/%m/%d %H:%M').astimezone(local_timezone)
     if now_time >= start_pause and now_time <= end_pause:
         time.sleep(60*5)
-        return True
-    else:
         return False
+    else:
+        return True
+
 
 def time_to_send():
-    delivery_time_ahead = datetime.now().astimezone(local_timezone) + timedelta(minutes=random.randrange(55, 60))
+    delivery_time_ahead = datetime.now().astimezone(local_timezone) + timedelta(minutes=55)
     delivery_str = datetime.strftime(delivery_time_ahead, '%Y/%m/%d %H:%M')
     return datetime.strptime(delivery_str, '%Y/%m/%d %H:%M').astimezone(local_timezone)
